8aa11af7984d11210cfdfc3965e124235b43e3dd
Kristiansito <kristianpro498@gmail.com>
2025-03-31 20:28:23 +0200

    Stub hecho de: - Piano - TeclaFactory - Pulsable

--- src/jd/piano/teclas/TeclaFactory.java
+++ src/jd/piano/teclas/TeclaFactory.java
@@ -1,4 +1,10 @@
 package jd.piano.teclas;
 
 public class TeclaFactory {
+    private TeclaFactory(){
+        throw new UnsupportedOperationException("No programado");
+    }
+    public static Tecla crearTecla(int nota){
+        throw new UnsupportedOperationException("No programado");
+    }
 }
