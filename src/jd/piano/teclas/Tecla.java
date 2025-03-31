f693c895ff3c3f4a54ee9aacb9d2d9f26f6d6582
Samuel <olivassamuel0@gmail.com>
2025-03-31 20:30:34 +0200

    Rama hecha para Tecla, TeclaBlanca, TeclaBlanca1/2/3 y Medible. Dejo las clases con Unssuported para que mis compañeros puedan trabajar con mis clases.

--- src/jd/piano/teclas/Tecla.java
+++ src/jd/piano/teclas/Tecla.java
@@ -1,4 +1,22 @@
 package jd.piano.teclas;
 
+import java.awt.*;
+
 public abstract class Tecla {
+    protected Point posicion;
+    private int nota;
+    private boolean pulsada;
+    private Color colorPulsada;
+    private Graphics graphics;
+
+    Tecla(int n){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    public int getNumeroNota(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    protected abstract int[] getVerticesX();
+    protected abstract int[] getVerticesY();
 }
--- src/jd/piano/teclas/TeclaBlanca.java
+++ src/jd/piano/teclas/TeclaBlanca.java
@@ -1,4 +1,10 @@
 package jd.piano.teclas;
 
 public abstract class TeclaBlanca {
+    public static final int ANCHURA = 25;
+    public static final int ALTURA = 100;
+
+    TeclaBlanca(int n){
+        throw new UnsupportedOperationException("Sin programar");
+    }
 }
