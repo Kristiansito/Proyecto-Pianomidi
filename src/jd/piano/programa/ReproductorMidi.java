aa76fddd155b53be8e787c45414644059bc42582
Kristiansito <kristianpro498@gmail.com>
2025-04-10 23:40:38 +0200

    ReproductorMidi medio hecho

--- src/jd/piano/programa/ReproductorMidi.java
+++ src/jd/piano/programa/ReproductorMidi.java
@@ -7,14 +7,17 @@ import javax.sound.midi.Receiver;
 import java.awt.*;
 
 public class ReproductorMidi implements Receiver {
-    private static final Color[] COLORES;
-    private PianoMidi piano;
+    private static final Color[] COLORES = {Color.CYAN, Color.BLUE, Color.GREEN, Color.MAGENTA,
+            Color.ORANGE, Color.PINK, Color.YELLOW, Color.RED, Color.DARK_GRAY, Color.LIGHT_GRAY,
+            new Color(64, 236, 127), new Color(227, 162, 84), new Color(174, 246, 12),
+            new Color(113, 172, 209), new Color(202, 184, 155), new Color(50, 142, 72)};
+    private Piano piano;
 
     public ReproductorMidi(){
-        throw new UnsupportedOperationException("No programado");
+        this.piano = null;
     }
     public void conectar(Piano p){
-        throw new UnsupportedOperationException("No programado");
+        this.piano = p;
     }
     public void reproducir(String ruta){
         throw new UnsupportedOperationException("No programado");
@@ -26,7 +29,5 @@ public class ReproductorMidi implements Receiver {
     }
 
     @Override
-    public void close() {
-        throw new UnsupportedOperationException("No programado");
-    }
+    public void close() {}
 }
