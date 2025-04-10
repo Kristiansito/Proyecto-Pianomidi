d6163a16b9fe0ebe3594c6c9cd4d77f6d183d8a3
Kristiansito <kristianpro498@gmail.com>
2025-04-11 00:26:00 +0200

    ReproductorMidi TERMINADO

--- src/jd/piano/programa/ReproductorMidi.java
+++ src/jd/piano/programa/ReproductorMidi.java
@@ -1,16 +1,18 @@
 package jd.piano.programa;
 
 import jd.piano.teclas.Piano;
+import jd.piano.teclas.Tecla;
 
-import javax.sound.midi.MidiMessage;
-import javax.sound.midi.Receiver;
+import javax.sound.midi.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class ReproductorMidi implements Receiver {
-    private static final Color[] COLORES = {Color.CYAN, Color.BLUE, Color.GREEN, Color.MAGENTA,
-            Color.ORANGE, Color.PINK, Color.YELLOW, Color.RED, Color.DARK_GRAY, Color.LIGHT_GRAY,
-            new Color(64, 236, 127), new Color(227, 162, 84), new Color(174, 246, 12),
-            new Color(113, 172, 209), new Color(202, 184, 155), new Color(50, 142, 72)};
+    private static final Color[] COLORES = {Color.MAGENTA, Color.DARK_GRAY, Color.PINK, Color.RED, Color.LIGHT_GRAY,
+            Color.YELLOW, Color.ORANGE, Color.CYAN, Color.GREEN, Color.BLUE,
+            new Color(135, 28, 219), new Color(75, 192, 183), new Color(255, 87, 51),
+            new Color(199, 0, 139), new Color(144, 238, 144), new Color(255, 215, 0)};
     private Piano piano;
 
     public ReproductorMidi(){
@@ -20,12 +22,54 @@ public class ReproductorMidi implements Receiver {
         this.piano = p;
     }
     public void reproducir(String ruta){
-        throw new UnsupportedOperationException("No programado");
+        try{
+            Sequence cancionMidi = MidiSystem.getSequence(new File(ruta));
+
+            Sequencer secuenciador = MidiSystem.getSequencer();
+            secuenciador.open();
+
+            Transmitter cable = secuenciador.getTransmitter();
+
+            cable.setReceiver(this);
+            secuenciador.setSequence(cancionMidi);
+
+            secuenciador.start();
+
+            Thread.sleep(cancionMidi.getMicrosecondLength()/1000);
+
+            cable.close();
+            secuenciador.close();
+
+        } catch (IOException | InvalidMidiDataException | MidiUnavailableException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void send(MidiMessage message, long timeStamp) {
-        throw new UnsupportedOperationException("No programado");
+        if (message instanceof ShortMessage) {
+            ShortMessage sm = (ShortMessage) message;
+            int nCanal = sm.getChannel();
+            if (nCanal != 9) {
+                int nNotaMusical = sm.getData1();
+                Tecla tecla = this.piano.getTecla(nCanal, nNotaMusical);
+                if (tecla != null) {
+                    int nComando = sm.getCommand();
+                    if (nComando == ShortMessage.NOTE_ON) {
+                        int volumen = sm.getData2();
+                        if (volumen > 0) {
+                            tecla.setColorPulsado(COLORES[nCanal]);
+                            tecla.pulsar();
+                        } else {
+                            tecla.soltar();
+                        }
+                    } else {
+                        tecla.soltar();
+                    }
+                    tecla.dibujar();
+                }
+            }
+        }
     }
 
     @Override
